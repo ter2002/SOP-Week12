82c19afccc159dc367a634fdd84c31bbc26c1f5e
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-11-22 11:12:04 +0700

    All in week12

---
+++ sop-lab12/auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.entities.AuthRequest;
+import com.example.authservice.entities.AuthResponse;
+import com.example.authservice.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(final AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<AuthResponse> logIn(@RequestBody AuthRequest authRequest){
+        return ResponseEntity.ok(authService.logIn(authRequest));
+    }
+}
